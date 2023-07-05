f17d9574a4e7c381aed97e77fe69f9af97166bca
Kairedblue <th8854427@gmail.com>
2023-07-05 09:51:37 +0700

    first commit

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM node:alpine 
+
+EXPOSE 5000
+
+ENV MONGODB_URI=mongodb://root:password123@mongodb-primary
+
+WORKDIR /usr/app
+
+RUN npm i npm@latest -g
+
+COPY package.json package-lock.json ./
+
+RUN npm install
+
+COPY . .
+
+CMD ["npm", "start"]
---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const http = require("http");
+const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+const path = require("path");
+const createError = require("http-errors");
+
+require("dotenv").config();
+
+const connectDB = require("./src/configs/mongodb.config");
+connectDB();
+
+const app = express();
+const server = http.createServer(app);
+const PORT = process.env.PORT || 5000;
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+app.use(helmet());
+app.use(express.static(path.join(__dirname, "public")));
+app.use((req, res, next) => {
+  next(createError.NotFound("This route does not exist"));
+});
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send({
+    status: err.status || 500,
+    message: err.message,
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
