4f22100e2953c1925f27e98b8ae3524afd9d2877
Kairedblue <th8854427@gmail.com>
2023-07-07 13:18:48 +0700

    Improvement

--- index.js
+++ index.js
@@ -12,19 +12,6 @@ const connectDB = require("./src/configs/mongodb.config");
 const redisClient = require("./src/configs/redis.config");
 connectDB();
 
-redisClient.set("foo", "bar", (err, reply) => {
-  if (err) {
-    console.log(err);
-  }
-  console.log(reply);
-});
-redisClient.get("foo", (err, reply) => {
-  if (err) {
-    console.log(err);
-  }
-  console.log(reply);
-});
-
 const app = express();
 const server = http.createServer(app);
 const PORT = process.env.PORT || 5000;
@@ -35,6 +22,28 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use(helmet());
 app.use(express.static(path.join(__dirname, "public")));
+app
+  .route("/api")
+  .get((req, res) => {
+    const data = redisClient.get("data", (err, data) => {
+      if (err) {
+        console.log(err);
+        throw err;
+      }
+      res.send(data);
+    });
+  })
+  .post((req, res) => {
+    const data = req.body.data;
+    redisClient.set("data", data, (err, data) => {
+      if (err) {
+        console.log(err);
+        throw err;
+      }
+      console.log(data);
+      res.send("Data saved to Redis");
+    });
+  });
 app.use((req, res, next) => {
   next(createError.NotFound("This route does not exist"));
 });
