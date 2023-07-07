5b57fe29775549245b246b1ff026445ab4502adc
Kairedblue <th8854427@gmail.com>
2023-07-07 12:45:15 +0700

    update the docker 10

---
+++ src/configs/redis.config.js
@@ -0,0 +1,17 @@
+const redis = require("redis");
+
+const redisClient = redis.createClient({
+  url: process.env.REDIS_URI,
+});
+
+redisClient.on("error", (err) => {
+  console.log("Error: " + err);
+});
+
+redisClient.on("ready", () => {
+  console.log("Redis client ready");
+});
+redisClient.on("connect", () => {
+  console.log("Redis client connected");
+});
+module.exports = redisClient;
